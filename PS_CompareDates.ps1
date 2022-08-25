e375a65b74afd09ae91c1de41840880543979948
marinkoco <69796741+marinkoco@users.noreply.github.com>
2022-08-25 18:42:16 +0200

    Update PS_CompareDates.ps1

--- PS_CompareDates.ps1
+++ PS_CompareDates.ps1
@@ -1,3 +1,5 @@
+#This PS script compares last write date to a file with today's date and  writes the result (0 or 1, with the timestamp of the action) to a log.
+
 $LMD = (Get-Item "<path to a file>").LastWriteTime.ToString("MM/dd/yyyy")
 
 $Today_Stamp = Get-Date -Format "MM/dd/yyyy"
